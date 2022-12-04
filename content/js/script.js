ed1701be028c2cdf336cbc1c8259ce9cc76acdf0
Abdulrahman Emad <65741884+abdulrahman3mad@users.noreply.github.com>
2022-12-04 14:17:42 +0200

    all

---
+++ content/js/script.js
@@ -0,0 +1,11 @@
+let freeTrialsPlans = document.querySelector(".free-trials");
+let NonTrialsPlans = document.querySelector(".non-free-trials");
+let toggleCheckBox = document.querySelector("#plan-type");
+
+toggleCheckBox.addEventListener("click", function () {
+    freeTrialsPlans.classList.toggle("active")
+    NonTrialsPlans.classList.toggle("active")
+    Array.from(NonTrialsPlans.querySelectorAll(".plan")).forEach((plan)=> plan.classList.add("wow"))
+})
+
+
